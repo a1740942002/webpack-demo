78ccd5027faac5313ee98e211dc1ebab493cd66a
a1740942002 <a1740942002@me.com>
2022-12-07 23:00:36 +0800

    add html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,47 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = {
+  target: 'web',
   entry: {
     index: './src/index.ts',
     print: './src/print.ts'
   },
+  mode: 'development',
   devtool: 'inline-source-map',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  // loader
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.gif/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
       }
     ]
   },
@@ -21,12 +50,12 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management'
-    })
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  }
+      title: 'Custom template',
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'index.[hash].css'
+    }),
+    new CompressionPlugin()
+  ]
 }
